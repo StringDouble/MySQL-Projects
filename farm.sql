224d9658730974fb255154d85c3a0c1cf3cedade
Yoana <yoana@Yoanas-MacBook-Air.local>
2018-07-21 02:35:36 +0300

    update

---
+++ farm.sql
@@ -0,0 +1,8 @@
+CREATE TABLE farm ( id INTEGER PRIMARY KEY, kind TEXT, count INTEGER);
+
+INSERT INTO farm VALUES (1, "chickens", 10);
+INSERT INTO farm VALUES (2, "horses", 3);
+INSERT INTO farm VALUES (3, "pigs", 5);
+
+SELECT * FROM farm;
+
